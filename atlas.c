c73ba6855e013e9aca0c1bd0cf141448c73a6c7f
ThejusGSajan <thejusgsajan@outlook.com>
2023-03-31 23:11:14 +0530

    refactor keypress, clear screen, reposition cursor

--- atlas.c
+++ atlas.c
@@ -1,24 +1,72 @@
+#include <ctype.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <termios.h>
 
+#define CTRL_KEY(k) ((k) & 0x1f)
 struct termios orig_termios;
+void kill(const char *s)
+{
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+    perror(s);
+    exit(1);
+}
 void disableRaw()
 {
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+        kill("tcsetattr");
 }
 void allowRaw()
 {
-    tcgetattr(STDIN_FILENO, &orig_termios);
+    if(tcgetattr(STDIN_FILENO, &orig_termios) == -1)
+        kill("tcgetattr");
     atexit(disableRaw);
     struct termios raw = orig_termios;
-    raw.c_lflag &= ~(ECHO);
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    raw.c_iflag &= ~(IXON | ICRNL | BRKINT | INPCK | ISTRIP);
+    raw.c_oflag &= ~(OPOST);
+    raw.c_cflag |= (CS8);
+    raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1)
+        kill("tcsetattr");
+}
+char readKey()
+{
+    int nread;
+    char c;
+    while((nread = read(STDIN_FILENO, &c, 1)) != 1)
+    {
+        if(nread == -1 && errno != EAGAIN)
+            kill("read");
+    }
+    return c;
+}
+void processKey()
+{
+    char c = readKey();
+    switch(c)
+    {
+        case CTRL_KEY('q'): write(STDOUT_FILENO, "\x1b[2J", 4);
+                            write(STDOUT_FILENO, "\x1b[H", 3);
+                            exit(0);
+                            break;
+    }
+}
+void refresh()
+{
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
 }
 void main()
 {
     allowRaw();
-    char c;
-    while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+    while (1)
+    {    
+        refresh();
+        processKey();
+    }
 }
