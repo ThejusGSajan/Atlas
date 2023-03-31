bc015bd5bf125a8380e2f50d9b4c9c0acccaacd3
ThejusGSajan <thejusgsajan@outlook.com>
2023-03-31 23:57:50 +0530

    add tildes(~)

--- atlas.c
+++ atlas.c
@@ -4,9 +4,16 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <termios.h>
+#include <sys/ioctl.h>
 
 #define CTRL_KEY(k) ((k) & 0x1f)
-struct termios orig_termios;
+struct config 
+{
+    int rows;
+    int cols;
+    struct termios orig_termios;
+};
+struct config C;
 void kill(const char *s)
 {
     write(STDOUT_FILENO, "\x1b[2J", 4);
@@ -16,15 +23,15 @@ void kill(const char *s)
 }
 void disableRaw()
 {
-    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &C.orig_termios) == -1)
         kill("tcsetattr");
 }
 void allowRaw()
 {
-    if(tcgetattr(STDIN_FILENO, &orig_termios) == -1)
+    if(tcgetattr(STDIN_FILENO, &C.orig_termios) == -1)
         kill("tcgetattr");
     atexit(disableRaw);
-    struct termios raw = orig_termios;
+    struct termios raw = C.orig_termios;
     raw.c_iflag &= ~(IXON | ICRNL | BRKINT | INPCK | ISTRIP);
     raw.c_oflag &= ~(OPOST);
     raw.c_cflag |= (CS8);
@@ -45,6 +52,43 @@ char readKey()
     }
     return c;
 }
+int getCursorLoc(int *rows, int *cols)
+{
+    char buf[32];
+    unsigned int i = 0;
+    if(write(STDOUT_FILENO, "\x1b[6n", 4) != 4)
+        return -1;
+    while(i < sizeof(buf) - 1)
+    {
+        if(read(STDIN_FILENO, &buf[i], 1) != 1)
+            break;
+        if(buf[i] == 'R')
+            break;
+        i++;
+    }
+    buf[i] = '\0';
+    if (buf[0] != '\x1b' || buf[1] != '[')
+        return -1;
+    if (sscanf(&buf[2], "%d;%d", rows, cols) != 2)
+        return -1;
+    return 0;
+}
+int getWindowSize(int *rows, int *cols)
+{
+    struct winsize ws;
+    if(ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0)
+    {    
+        if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12)
+            return -1;
+        return getCursorLoc(rows, cols);
+    }
+    else
+    {
+        *rows = ws.ws_row;
+        *cols = ws.ws_col;
+        return 0;
+    }
+}
 void processKey()
 {
     char c = readKey();
@@ -56,10 +100,27 @@ void processKey()
                             break;
     }
 }
+void drawRows()
+{
+    int i;
+    for (i = 0; i < C.rows; i++)
+    {    
+        write(STDOUT_FILENO, "~", 1);
+        if (i < C.rows - 1)
+            write(STDOUT_FILENO, "\r\n", 2);
+    }
+}
 void refresh()
 {
     write(STDOUT_FILENO, "\x1b[2J", 4);
     write(STDOUT_FILENO, "\x1b[H", 3);
+    drawRows();
+    write(STDOUT_FILENO, "\x1b[H", 3);
+}
+void init()
+{
+    if(getWindowSize(&C.rows, &C.cols) == -1)
+        kill("getWindowSize");
 }
 void main()
 {
