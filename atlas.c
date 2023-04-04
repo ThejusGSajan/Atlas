d3e539003ae0424524dd468a8de5bd0205db5723
ThejusGSajan <thejusgsajan@outlook.com>
2023-04-04 20:28:25 +0530

    add page up, page down, home and end keys

--- atlas.c
+++ atlas.c
@@ -15,7 +15,11 @@ enum key
     ARROW_UP,
     ARROW_LEFT = 1000,
     ARROW_DOWN,
-    ARROW_RIGHT
+    ARROW_RIGHT,
+    HOME,
+    END,
+    PAGE_UP,
+    PAGE_DOWN
 };
 struct config 
 {
@@ -71,12 +75,42 @@ int readKey()
             return '\x1b';
         if(seq[0] == '[')
         {
-            switch (seq[1])
+            if(seq[1] >= '0' && seq[1] <= '9')
             {
-                case 'A': return ARROW_UP;
-                case 'B': return ARROW_DOWN;
-                case 'C': return ARROW_RIGHT;
-                case 'D': return ARROW_LEFT;
+                if(read(STDIN_FILENO, &seq[2], 1) != 1)
+                    return '\x1b';
+                if(seq[2] == '~')
+                {
+                    switch(seq[1])
+                    {
+                        case '1': return HOME;
+                        case '4': return END;
+                        case '5': return PAGE_UP;
+                        case '6': return PAGE_DOWN;
+                        case '7': return HOME;
+                        case '8': return END;
+                    }
+                }
+            }
+            else
+            {
+                switch (seq[1])
+                {
+                    case 'A': return ARROW_UP;
+                    case 'B': return ARROW_DOWN;
+                    case 'C': return ARROW_RIGHT;
+                    case 'D': return ARROW_LEFT;
+                    case 'H': return HOME;
+                    case 'F': return END;
+                }
+            }
+        }
+        else if (seq[0] == '0')
+        {
+            switch(seq[1])
+            {
+                case 'H': return HOME;
+                case 'F': return END;
             }
         }
         return '\x1b';
@@ -146,13 +180,17 @@ void moveCursor(int key)
 {
     switch (key)
     {
-        case ARROW_UP:      C.y--;
+        case ARROW_UP:      if(C.y != 0)
+                                C.y--;
                             break;
-        case ARROW_LEFT:    C.x--;
+        case ARROW_LEFT:    if(C.x != 0)
+                                C.x--;
                             break;
-        case ARROW_DOWN:    C.y++;
+        case ARROW_DOWN:    if(C.y != C.rows - 1)
+                                C.y++;
                             break;
-        case ARROW_RIGHT:   C.x++;
+        case ARROW_RIGHT:   if(C.x != C.cols -1)
+                                C.x++;
                             break;
     }
 }
@@ -165,6 +203,17 @@ void processKey()
                             write(STDOUT_FILENO, "\x1b[H", 3);
                             exit(0);
                             break;
+        case HOME:          C.x = 0;
+                            break;
+        case END:           C.x = C.cols - 1;
+                            break;
+        case PAGE_UP:
+        case PAGE_DOWN:     {
+                                int times = C.rows;
+                                while(times--)
+                                    moveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+                                break;
+                            }
         case ARROW_UP:
         case ARROW_LEFT:
         case ARROW_DOWN:
